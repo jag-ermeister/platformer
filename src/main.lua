014125d57d1c0c48bcbb70bc88d9b4634fa683d6
Jeremy Gustine <jeremy@crafted.solutions>
2020-10-23 19:59:30 -0600

    First screen

--- src/main.lua
+++ src/main.lua
@@ -6,8 +6,12 @@ end
  
 function _draw()
   cls(12)
+  print("")
+  print("")
+  print("  Yo, I'm a ninja.")
+  print("  I'm a motha fuckin' ninja.")
   palt(0, false)
   palt(12, true)
   map(0,0)
-  spr(4)
+  spr(1, 16, 120 - 8)
 end

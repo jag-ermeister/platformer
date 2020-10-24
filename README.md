9f79e6c0b393138c0f2e8a6be913657a49b0eff6
Jeremy Gustine <jeremy@crafted.solutions>
2020-10-24 13:06:53 -0600

    Add basic map collision

--- README.md
+++ README.md
@@ -6,5 +6,10 @@
 ### steps
 ---------
 - simple movement (move everywhere, no gravity)
-- map collision
-- 
+- simple map collision
+    - mark map tiles that should be solid
+    - detect if player sprite is overlapping a solid map tile
+    - reset player to old position if colliding with solid map
+- add better movement
+    - physics
+- camera control
--- src/main.lua
+++ src/main.lua
@@ -1,26 +1,31 @@
 function _init()
   player = {
-    sp=1,
-    x=16,
-    y=112,
-    w=8,
-    h=8,
-    dx=0,
-    dy=0,
-    max_dx=2,
-    max_dy=3,
-    acc=0.5, -- horizontal acceleration
-    boost=4, -- vertical acceleration
-    anim=0,
-    running=false,
-    jumping=false,
-    falling=false,
-    sliding=false,
-    landed=false
+    sp = 1,
+    x = 16,
+    y = 112,
+    old_x = 16,
+    old_y = 112,
+    w = 8,
+    h = 8,
+    dx = 0,
+    dy = 0,
+    max_dx = 2,
+    max_dy = 3,
+    acc = 0.5, -- horizontal acceleration
+    boost = 4, -- vertical acceleration
+    anim = 0,
+    running = false,
+    jumping = false,
+    falling = false,
+    sliding = false,
+    landed = false
   }
 end
   
 function _update()
+  player.old_x = player.x
+  player.old_y = player.y
+  
   if btn(0) then --left
     player.x -=1
   end
@@ -32,6 +37,29 @@ function _update()
   end
   if btn(3) then --down
     player.y +=1
+  end
+
+  if map_collide(player) then
+    player.x = player.old_x
+    player.y = player.old_y
+  end
+end
+
+function map_collide(obj)
+  --mget gives us the sprite assigned to a cell on the map
+  -- must convert pixel to cell
+  s1 = mget(obj.x / obj.w, obj.y / obj.h)
+  s2 = mget((obj.x + obj.w - 1) / obj.w, obj.y / obj.h)
+  s3 = mget(obj.x / obj.w, (obj.y + obj.w - 1) / obj.h)
+  s4 = mget((obj.x + obj.w - 1) / obj.w, (obj.y + obj.w - 1) / obj.h)
+  
+  if fget(s1, 0)
+  or fget(s2, 0)
+  or fget (s3, 0)
+  or fget (s4, 0) then
+    return true
+  else
+    return false
   end
 end
  
